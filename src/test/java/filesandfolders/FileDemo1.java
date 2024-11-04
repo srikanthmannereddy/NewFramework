d0877b42eddd620997dc55915998861901392f17
Srikanth Reddy Manne <mannesrikanthreddy09@gmail.com>
2024-11-04 21:55:17 +0530

    first commit

---
+++ src/test/java/filesandfolders/FileDemo1.java
@@ -0,0 +1,11 @@
+package filesandfolders;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FileDemo1 {
+	
+	public static void main (String [] args) {
+		new ChromeDriver();
+	}
+
+}
---
+++ src/test/java/regression/ValidateLogin.java
@@ -0,0 +1,16 @@
+package regression;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ValidateLogin {
+	
+	@Test
+	public void ValidateLoginTest()
+	
+	{
+		new ChromeDriver();
+		
+	}
+
+}
